67c4e1ddb92090eff5ac321d9c896a26b8a2f5b5
rohit <rohit@yogems.com>
2020-04-07 18:54:04 +0530

    master

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,14 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: [
     './home.component.css',
-    '../../../theme/assets/css/style.css',
     '../../../theme/assets/vendor/bootstrap/css/bootstrap.min.css',
     '../../../theme/assets/vendor/icofont/icofont.min.css',
     '../../../theme/assets/vendor/boxicons/css/boxicons.min.css',
+    '../../../theme/assets/vendor/animate.css/animate.min.css',
     '../../../theme/assets/vendor/venobox/venobox.css',
     // '../../../theme/assets/vendor/remixicon/remixicon.css',
     '../../../theme/assets/vendor/owl.carousel/assets/owl.carousel.min.css',
     // '../../../theme/assets/vendor/aos/aos.css'
+    '../../../theme/assets/css/style.css'
   ]
 })
 export class HomeComponent implements OnInit {
